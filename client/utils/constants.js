f28b379ad6fe4ee2af68263eb8208fb0b1a1bb48
sanumuhammedc <sanumuhammed.c@gmail.com>
2022-08-28 07:47:19 +0530

    about to finish

---
+++ client/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Betting.json';
+
+export const contractABI = abi.abi; 
+export const contractAddress = '0xF827BEb39EfBdCE59c390A258DA70C8Fdd526111';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-rinkeby.alchemyapi.io/v2/JIqMiOvC7MrIpHH_KTP7AupfFMycbbAV
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.9',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/JIqMiOvC7MrIpHH_KTP7AupfFMycbbAV',
+      accounts: ['7bd034bc686d00d7b2f979de097f967a1dcf3f0c868577e44fe5daaa3421b516']
+    }
+  }
+}
